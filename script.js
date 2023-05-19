4401ff2f91b865bbf19ba2f763e2f67ffa3ea9b6
Nao Aoyama <ea80002@gmail.com>
2023-05-18 20:01:11 -0700

    firstcommit

---
+++ script.js
@@ -0,0 +1,15 @@
+// ハンバーガーメニュー
+$(function() {
+  
+    $('.hamburger').click(function() {
+        $(this).toggleClass('active');
+  
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
+    });
+  
+  });
+  
